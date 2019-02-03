0bae17b59eaf11451ed20f61106b27e0ad957520
Kevin Martinez <kevin.martinezc@hotmail.com>
2019-02-02 23:24:40 -0500

    add slides

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SlidesPage } from '../slides/slides';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openSlides(){
+    this.navCtrl.push(SlidesPage);
   }
 
 }
